fd839c090802769fa2866fff8693e25cee215972
afarchant <afarchant@gmail.com>
2016-11-11 12:25:35 -0500

    Add files via upload

---
+++ Galcon.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 11 11:43:30 2016
+
+@author: Chant Afarian
+"""
+
+
+def makeOrder(planets, ships, turnNumber, myID):
+    myplan = []
+    planeut = []
+    planene = []
+    ship = []
+    turnnum = []
+    ID = []
+    for planet in self.planets:
+        if planet.planetOwner == 1:
+            myplan.append[planet]
+        elif planet.planetOwner == 2:
+            planene.append[planet]
+        else:
+            planene.append[planet]
+
+    
+        
+     
